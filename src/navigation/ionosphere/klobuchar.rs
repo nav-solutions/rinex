7971fdaf4b0b31d0fdd77a40fc70e8329f1d3c12
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-11-01 10:09:36 +0100

    update gnsslib
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/navigation/ionosphere/klobuchar.rs
+++ src/navigation/ionosphere/klobuchar.rs
@@ -227,7 +227,7 @@ impl KbModel {
             )
         } else {
             format!(
-                "{:X}A  {} {} {} {}",
+                "{:x}A  {} {} {} {}",
                 constellation,
                 NavFormatter::new_iono_alpha_beta(self.alpha.0),
                 NavFormatter::new_iono_alpha_beta(self.alpha.1),
@@ -257,7 +257,7 @@ impl KbModel {
             )
         } else {
             format!(
-                "{:X}B  {} {} {} {}",
+                "{:x}B  {} {} {} {}",
                 constellation,
                 NavFormatter::new_iono_alpha_beta(self.beta.0),
                 NavFormatter::new_iono_alpha_beta(self.beta.1),
