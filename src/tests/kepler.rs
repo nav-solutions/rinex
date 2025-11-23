e666a78ee5f6b903a812371b88304047060285aa
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-11-23 09:29:00 +0100

    add test, fix geosat case
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/tests/kepler.rs
+++ src/tests/kepler.rs
@@ -19,15 +19,162 @@ use std::{
 };
 
 #[test]
+fn v3_ephemeris_selection() {
+    let dut = Rinex::from_gzip_file("data/NAV/V3/MOJN00DNK_R_20201770000_01D_MN.rnx.gz").unwrap();
+
+    for (epoch_str, satellite_str, expected_toc_str) in [
+        ("2020-06-24T22:00:00 BDT", "C05", "2020-06-24T22:00:00 BDT"),
+        ("2020-06-24T22:00:01 BDT", "C05", "2020-06-24T22:00:00 BDT"),
+        ("2020-06-24T21:59:59 BDT", "C05", "2020-06-24T22:00:00 BDT"),
+        ("2020-06-24T21:00:00 BDT", "C05", "2020-06-24T22:00:00 BDT"),
+        ("2020-06-24T22:00:02 BDT", "C05", "2020-06-24T22:00:00 BDT"),
+        ("2020-06-24T22:15:02 BDT", "C05", "2020-06-24T22:00:00 BDT"),
+        ("2020-06-24T23:00:00 BDT", "C05", "2020-06-24T23:00:00 BDT"),
+        (
+            "2020-06-25T00:00:00 GPST",
+            "G30",
+            "2020-06-25T00:00:00 GPST",
+        ),
+        (
+            "2020-06-25T00:00:01 GPST",
+            "G30",
+            "2020-06-25T00:00:00 GPST",
+        ),
+        (
+            "2020-06-25T00:00:09 GPST",
+            "G30",
+            "2020-06-25T00:00:00 GPST",
+        ),
+        (
+            "2020-06-25T01:15:09 GPST",
+            "G30",
+            "2020-06-25T02:00:00 GPST",
+        ),
+        (
+            "2020-06-25T01:15:09 GPST",
+            "G30",
+            "2020-06-25T02:00:00 GPST",
+        ),
+        (
+            "2020-06-25T01:45:09 GPST",
+            "G30",
+            "2020-06-25T02:00:00 GPST",
+        ),
+        (
+            "2020-06-25T01:59:58 GPST",
+            "G30",
+            "2020-06-25T02:00:00 GPST",
+        ),
+        (
+            "2020-06-25T01:59:59 GPST",
+            "G30",
+            "2020-06-25T02:00:00 GPST",
+        ),
+        (
+            "2020-06-25T02:00:00 GPST",
+            "G30",
+            "2020-06-25T02:00:00 GPST",
+        ),
+        (
+            "2020-06-25T02:00:01 GPST",
+            "G30",
+            "2020-06-25T02:00:00 GPST",
+        ),
+        ("2020-06-24T22:40:00 GST", "E04", "2020-06-24T22:40:00 GST"),
+        ("2020-06-24T22:40:01 GST", "E04", "2020-06-24T22:40:00 GST"),
+        ("2020-06-24T22:39:59 GST", "E04", "2020-06-24T22:40:00 GST"),
+        ("2020-06-24T22:44:59 GST", "E04", "2020-06-24T22:40:00 GST"),
+        ("2020-06-24T22:45:00 GST", "E04", "2020-06-24T22:40:00 GST"),
+        ("2020-06-24T22:45:01 GST", "E04", "2020-06-24T22:50:00 GST"),
+        ("2020-06-24T22:49:58 GST", "E04", "2020-06-24T22:50:00 GST"),
+        ("2020-06-24T22:49:59 GST", "E04", "2020-06-24T22:50:00 GST"),
+        (
+            "2020-06-25T01:40:16 GPST",
+            "S44",
+            "2020-06-25T01:40:16 GPST",
+        ),
+        (
+            "2020-06-25T01:40:15 GPST",
+            "S44",
+            "2020-06-25T01:40:16 GPST",
+        ),
+        (
+            "2020-06-25T01:40:17 GPST",
+            "S44",
+            "2020-06-25T01:40:16 GPST",
+        ),
+        (
+            "2020-06-25T01:42:20 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+        (
+            "2020-06-25T01:42:21 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+        (
+            "2020-06-25T01:42:22 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+        (
+            "2020-06-25T01:42:23 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+        (
+            "2020-06-25T01:42:24 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+        (
+            "2020-06-25T01:42:25 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+        (
+            "2020-06-25T01:42:26 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+        (
+            "2020-06-25T01:42:27 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+        (
+            "2020-06-25T01:42:28 GPST",
+            "S44",
+            "2020-06-25T01:42:24 GPST",
+        ),
+    ] {
+        let epoch = Epoch::from_str(epoch_str).unwrap();
+        let satellite = SV::from_str(satellite_str).unwrap();
+        let expected_toc = Epoch::from_str(expected_toc_str).unwrap();
+
+        let (toc, _toe, _eph) = dut
+            .nav_satellite_ephemeris_selection(satellite, epoch)
+            .unwrap();
+
+        assert_eq!(
+            toc, expected_toc,
+            "test failed for {}@{}",
+            satellite_str, epoch_str
+        );
+    }
+}
+
+#[test]
 fn v3_kepler() {
     init_logger();
-
     let g10 = SV::from_str("G10").unwrap();
     let e30 = SV::from_str("E30").unwrap();
     let c10 = SV::from_str("C10").unwrap();
 
     let dut = Rinex::from_gzip_file("data/NAV/V3/MOJN00DNK_R_20201770000_01D_MN.rnx.gz").unwrap();
 
+    // test navigation
     for (t_gpst, x_km, y_km, z_km) in [
         (
             "2020-06-25T02:00:00 GPST",
@@ -50,7 +197,7 @@ fn v3_kepler() {
     ] {
         let t_gpst = Epoch::from_str(t_gpst).unwrap();
 
-        let (_, _, eph) = dut.nav_ephemeris_selection(g10, t_gpst).unwrap();
+        let (_, _, eph) = dut.nav_satellite_ephemeris_selection(g10, t_gpst).unwrap();
 
         let orbit = eph.resolve_orbital_state(g10, t_gpst, 10).unwrap();
 
@@ -104,7 +251,7 @@ fn v3_kepler() {
     ] {
         let t_gpst = Epoch::from_str(t_gpst).unwrap();
 
-        let (_, _, eph) = dut.nav_ephemeris_selection(e30, t_gpst).unwrap();
+        let (_, _, eph) = dut.nav_satellite_ephemeris_selection(e30, t_gpst).unwrap();
 
         let orbit = eph.resolve_orbital_state(e30, t_gpst, 10).unwrap();
 
@@ -170,7 +317,7 @@ fn v3_kepler() {
     ] {
         let t_gpst = Epoch::from_str(t_gpst).unwrap();
 
-        let (_, _, eph) = dut.nav_ephemeris_selection(c10, t_gpst).unwrap();
+        let (_, _, eph) = dut.nav_satellite_ephemeris_selection(c10, t_gpst).unwrap();
 
         let orbit = eph.resolve_orbital_state(c10, t_gpst, 10).unwrap();
 
